32973607059ad9c8385ac4bf73481e9266812a7d
Andry <andry@coderwise.com>
2025-05-20 11:34:15 -0400

    - enable auth-api publishing, temporary modified deploy script to publish auth-api

--- core-auth/auth-api/build.gradle.kts
+++ core-auth/auth-api/build.gradle.kts
@@ -1,9 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinMultiplatformLibrary)
     alias(libs.plugins.kotlinSerialization)
 
-    id("maven-publish")
+    alias(libs.plugins.mavenPublish)
 }
 
 kotlin {
@@ -26,11 +28,41 @@ kotlin {
     }
 }
 
-publishing {
-    repositories {
-        mavenLocal()
-    }
-}
-
 group = "com.coderwise.core"
 version = libs.versions.coderwiseCore.get()
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    if (project.hasProperty("sign")) {
+        signAllPublications()
+    }
+
+    coordinates(group.toString(), "auth-api", version.toString())
+
+    pom {
+        name = "Core Auth API library"
+        description = "Core Auth API library"
+        inceptionYear = "2025"
+        url = "https://github.com/coderwise/core.mobile"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "coderwise"
+                name = "Coderwise"
+                url = "https://github.com/coderwise"
+            }
+        }
+        scm {
+            url = "https://github.com/coderwise/core.mobile"
+            connection = "https://github.com/coderwise/core.mobile.git"
+            developerConnection = "git://github.com/coderwise/core.mobile.git"
+        }
+    }
+}
